f9884ce02863f51d791a99ab2999bc1c2761ebe3
sakesensei <7018787+sakesensei@users.noreply.github.com>
2017-09-28 21:43:50 +0000

    Added classes file

---
+++ TextAdventureEngine/TextAdventureEngine/Classes.cs
@@ -0,0 +1,20 @@
+﻿namespace TextAdventureEngine
+{
+	// Main Class - Thing
+	public abstract class Thing
+	{
+		private string _name;
+		private string _description;
+
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+	}
+}
