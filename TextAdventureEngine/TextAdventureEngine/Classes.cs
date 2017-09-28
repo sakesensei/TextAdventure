790df983b34dabcd424af17373c738850a29969b
sakesensei <7018787+sakesensei@users.noreply.github.com>
2017-09-28 21:55:33 +0000

    Added more classes

--- TextAdventureEngine/TextAdventureEngine/Classes.cs
+++ TextAdventureEngine/TextAdventureEngine/Classes.cs
@@ -17,4 +17,54 @@
 			set { _description = value; }
 		}
 	}
+
+	public abstract class ThingHolder : Thing
+	{
+		
+	}
+
+	public class Room : ThingHolder
+	{
+		private int _x;
+		private int _y;
+
+		public int X
+		{
+			get { return _x; }
+			set { _x = value; }
+		}
+		public int Y
+		{
+			get { return _y; }
+			set { _y = value; }
+		}
+	}
+
+	// Actor - Player, Npc
+	public abstract class Actor : Thing
+	{
+		private bool _isalive;
+
+		public bool IsAlive
+		{ 
+			get { return _isalive; }
+			set { _isalive = value; }
+		}
+	}
+
+	// Items
+	public class Item
+	{
+		private bool _iscollectable;
+
+		public bool IsCollectable
+		{
+			get { return _iscollectable; }
+			set { _iscollectable = value; }
+		}
+
+		Item(string Name, string Description, bool IsCollectable)
+		{
+		}
+	}
 }
